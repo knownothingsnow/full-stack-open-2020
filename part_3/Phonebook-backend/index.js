802c61a1755ee49263bd32c21b6b25371d9a8475
Knight Yang <knightyoung3000@gmail.com>
2020-12-15 17:31:17 +0800

    feat: Exercises 3.3

--- part_3/Phonebook-backend/index.js
+++ part_3/Phonebook-backend/index.js
@@ -1,26 +1,26 @@
 const express = require('express')
 const app = express()
 
-let notes = [
+const persons = [
   {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
   },
   {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2
   },
   {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3
   },
   {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4
   }
 ]
 
@@ -28,13 +28,19 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/persons/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const person = persons.find((p) => p.id === id)
+  if (person) {
+    res.json(person).send()
+  } else {
+    res.status(404).end()
+  }
 })
 
 app.get('/info', (req, res) => {
   res.send(`
-    <div>Phonebook has info for ${notes.length} people</div>
+    <div>Phonebook has info for ${persons.length} people</div>
     <div>${new Date()}</div>
   `)
 })
