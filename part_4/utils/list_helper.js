7f71844f3d65066dcc141bc970a8f3ad73133e19
Knight Yang <knightyoung3000@gmail.com>
2021-01-29 22:27:29 +0800

    feat: Exercises 4.4

--- part_4/utils/list_helper.js
+++ part_4/utils/list_helper.js
@@ -1,8 +1,15 @@
-const dummy = (blogs) => {
+function dummy(blogs) {
   console.log(blogs);
   return 1
 }
 
+function totalLikes(list) {
+  return list.reduce((sum, curr) => {
+    return sum += curr.likes
+  }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
