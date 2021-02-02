77b4067b3711aa5073fd1bdd4d24d5a77c6c5d27
Knight Yang <knightyoung3000@gmail.com>
2021-02-02 21:53:41 +0800

    feat: Exercises 4.17

--- part_4/controllers/blogs.js
+++ part_4/controllers/blogs.js
@@ -1,29 +1,31 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/blogs', async (request, response) => {
-  const blogs = await Blog.find({})
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user')
   response.json(blogs)
 })
 
-blogRouter.post('/blogs', async (request, response, next) => {
-  if (request.title === undefined || request.url === undefined) {
+blogRouter.post('/', async (request, response, next) => {
+  if (!request.body.title || !request.body.url) {
     return response.status(400).send()
   }
   const blog = new Blog({
+    user: '60194fd61e04b3569f9bf0c5',
     likes: request.body.likes || 0,
     ...request.body
   })
-  const result = await blog.save()
-  response.status(201).json(result)
+  await blog.save()
+  const newBlogs = await Blog.populate(blog, { path: 'user' })
+  response.status(201).json(newBlogs)
 })
 
-blogRouter.delete('/blogs/:id', async function (req, res, next) {
+blogRouter.delete('/:id', async function (req, res, next) {
   await Blog.findByIdAndRemove(req.params.id)
   res.status(204).end()
 })
 
-blogRouter.put('/blogs/:id', async function (req, res, next) {
+blogRouter.put('/:id', async function (req, res, next) {
   const result = await Blog.findByIdAndUpdate(req.params.id, { ...req.body }, { new: true })
   res.json(result)
 })
