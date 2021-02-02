33902fff3cda53eddd569a94103c4e0c527c3589
Knight Yang <knightyoung3000@gmail.com>
2021-02-02 22:12:24 +0800

    feat: Exercises 4.18

--- part_4/app.js
+++ part_4/app.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
@@ -33,6 +34,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
