c5058170fa68907ca8ad5fa0e61854c35d1a88a9
Knight Yang <knightyoung3000@gmail.com>
2021-02-23 02:11:32 +0800

    feat: Exercises 5.7

--- part_5/src/components/Blog.jsx
+++ part_5/src/components/Blog.jsx
@@ -1,10 +1,33 @@
+/* eslint-disable */
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [extend, setExtend] = useState(false)
 
-/* eslint-disable  */
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleExtend = () => { setExtend(!extend) }
+
+  return (
+    <div style={blogStyle}>
+      <p>
+        {blog.title} {blog.author}
+        <button onClick={toggleExtend}>{extend? 'hide':'view'}</button>
+      </p>
+      {extend &&
+        <>
+          <p>{blog.url}</p>
+          <p>Likes:{blog.likes} <button>like</button></p>
+          <p>{blog.user?.username}</p>
+        </>
+      }
+    </div>
+  )
+}
 
 export default Blog
