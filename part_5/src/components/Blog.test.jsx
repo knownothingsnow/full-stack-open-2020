3984a3e9875afb23ba5b5d07fe182b4ce52f6349
Knight Yang <knightyoung3000@gmail.com>
2021-02-24 23:16:44 +0800

    feat: Exercises 5.14

--- part_5/src/components/Blog.test.jsx
+++ part_5/src/components/Blog.test.jsx
@@ -1,10 +1,29 @@
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import React from 'react'
 import Blog from './Blog'
 
-describe('test for blog component',() => {
+describe('test for blog component', () => {
+  const blog = {
+    title: 'test-title',
+    author: 'test-author',
+    url: 'test-url',
+    likes: 233,
+    id: '601ea1a16ed7f4f49c5d6668'
+  }
+
+  const component = render(
+    <Blog blog={blog} likeBlog={() => {}} removeBlog={() => {}}/>
+  )
+
   test('test right render', () => {
+    expect(component.container.querySelector('.blog-title')).toHaveTextContent('test-title')
+    expect(component.container.querySelector('.blog-author')).toHaveTextContent('test-author')
+    expect(component.container.querySelector('.blog-url')).toBeNull()
+    expect(component.container.querySelector('.blog-likes')).toBeNull()
+  })
+
+  test('show url and likes', () => {
     const blog = {
       title: 'test-title',
       author: 'test-author',
@@ -16,10 +35,8 @@ describe('test for blog component',() => {
     const component = render(
       <Blog blog={blog} likeBlog={() => {}} removeBlog={() => {}}/>
     )
-    // component.debug()
-    expect(component.container.querySelector('.blog-title')).toHaveTextContent('test-title')
-    expect(component.container.querySelector('.blog-author')).toHaveTextContent('test-author')
-    expect(component.container.querySelector('.blog-url')).toBeNull()
-    expect(component.container.querySelector('.blog-likes')).toBeNull()
-  })
+    fireEvent.click(component.getByText('view'))
+    expect(component.container.querySelector('.blog-url')).toHaveTextContent('test-url')
+    expect(component.container.querySelector('.blog-likes')).toHaveTextContent('Likes:233')
+  } )
 })
