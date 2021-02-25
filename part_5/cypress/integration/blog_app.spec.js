09224d8bfbe9270171b3e27abbfdfed74713678d
Knight Yang <knightyoung3000@gmail.com>
2021-02-26 02:23:07 +0800

    feat: Exercises 5.18

--- part_5/cypress/integration/blog_app.spec.js
+++ part_5/cypress/integration/blog_app.spec.js
@@ -1,6 +1,11 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.request('POST', 'http://localhost:3001/api/users', {
+      'username': 'zhang',
+      'name': 'san',
+      'password': '123'
+    })
     cy.visit('http://localhost:3000')
   })
 
@@ -8,8 +13,35 @@ describe('Blog app', function() {
     cy.contains('login required').should('not.visible')
     cy.contains('login here').click()
     cy.contains('login required').should('be.visible')
-    cy.get('form').find('input[name=username]')
-    cy.get('form').find('input[name=password]')
-    cy.get('form').children('button').contains('login')
+    cy.get('form').find('input[name=username]').as('username')
+    cy.get('form').find('input[name=password]').as('password')
+    cy.get('form').children('button').contains('login').as('loginBtn')
+  })
+
+  describe('Login', function () {
+    beforeEach(function () {
+      cy.get('form').find('input[name=username]').as('username')
+      cy.get('form').find('input[name=password]').as('password')
+      cy.get('form').children('button').contains('login').as('loginBtn')
+    })
+
+    it('succeeds with correct credentials', function() {
+      cy.contains('login here').click()
+      cy.get('@username').type('zhang')
+      cy.get('@password').type('123')
+      cy.get('@loginBtn').click()
+      cy.contains('zhang logged in')
+      cy.contains('logout!').click()
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('login here').click()
+      cy.get('@username').type('zhang')
+      cy.get('@password').type('something wrong')
+      cy.get('@loginBtn').click()
+      cy.get('.message.error')
+        .contains('wrong username or password')
+        .should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
