df4895082d36e1de51142a1d6dfaf60341824f68
Knight Yang <knightyoung3000@gmail.com>
2021-04-01 18:06:37 +0800

    feat: Exercises 6.3

--- part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -9,21 +9,39 @@ const anecdotesAtStart = [
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
-const asObject = (anecdote) => {
+const initialState = anecdotesAtStart.map((anecdote) => {
   return {
     content: anecdote,
     id: getId(),
     votes: 0
   }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
+})
 
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
-
+  switch (action.type) {
+    case 'add-vote': {
+      const id = action.data.id
+      const oldAnecdote = state.find(i => i.id === id)
+      const newAnecdote = {
+        ...oldAnecdote,
+        votes: oldAnecdote.votes+1
+      }
+      return state.map(i =>i.id !== id ? i : newAnecdote )
+    }
+  
+    default:
+      break;
+  }
   return state
+}
+
+export const addVote = (id) => {
+  return {
+    type: 'add-vote',
+    data: {id}
+  }
 }
 
 export default reducer
