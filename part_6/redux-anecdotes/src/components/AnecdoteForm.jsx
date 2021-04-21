79a2b01ec5a70e25d40e5011a81908ddefb76164
Knight Yang <knightyoung3000@gmail.com>
2021-04-21 18:15:59 +0800

    fix: add and vote a anecdote both show notification now

--- part_6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part_6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -4,13 +4,9 @@ import { create } from '../reducers/anecdoteReducer.js'
 import { newMsg } from '../reducers/notificationReducer.js'
 
 const AnecdoteForm = (props) => {
-  const showMsg = ({ content,delay}) => {
-    props.newMsg(content,delay)
-  }
-
   const newAnecdote = async (e) => {
     e.preventDefault()
-    showMsg({ content: 'new anecdote added', delay: 5 })
+    props.newMsg('new anecdote added', 5 )
     props.create(e.target.anecdote.value)
   }
 
--- part_6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part_6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect} from 'react'
 import { connect } from 'react-redux'
 import Filter from './Filter'
-import { addVote,initializeAnecdote } from '../reducers/anecdoteReducer.js'
+import { addVote, initializeAnecdote } from '../reducers/anecdoteReducer.js'
+import { newMsg } from '../reducers/notificationReducer.js'
 
 const AnecdoteList = (props) => {
   useEffect(() => {
     props.initializeAnecdote()
   }, [])
 
-  const vote = (id) => {
+  const vote = (id,content) => {
     props.addVote(id)
+    props.newMsg(`you voted '${content}'`,5)
   }
 
   return <>
@@ -21,7 +23,7 @@ const AnecdoteList = (props) => {
           </div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button onClick={() => vote(anecdote.id,anecdote.content)}>vote</button>
           </div>
         </div>
       )}
@@ -38,5 +40,6 @@ export default connect((state) => {
   }
  }, {
   initializeAnecdote,
-  addVote
+  addVote,
+  newMsg
 })(AnecdoteList)
